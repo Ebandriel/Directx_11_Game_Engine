f36a7eb18fdef6319fd714fe644553c94f3e4dcc
Ebandriel <67708633+Ebandriel@users.noreply.github.com>
2020-07-12 16:57:07 +0100

    clean up

---
+++ Engine.cpp
@@ -0,0 +1,7 @@
+#include "Engine.h"
+
+bool Engine::Initialize(HINSTANCE hInstance, std::string window_title, std::string window_class, int width, int height)
+{
+    return this->render_window.Initialize(hInstance, window_title, window_class, width, height);
+   
+}
